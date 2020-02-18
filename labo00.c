cc8bf323101af831a8711864a48e4dea6d8b661e
GautheyArnaud <61149055+GautheyArnaud@users.noreply.github.com>
2020-02-18 23:28:30 +0100

    Update labo00.c

--- labo00.c
+++ labo00.c
@@ -1,1 +1,37 @@
-//blablabla
+#define SIZE 100 //taille phrases
+#define _CRT_SECURE_NO_WARNINGS//uniquement sous windows
+#define ViderBuffer  while(getchar() != '\n') // fonction qui vide le buffer
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+	char phrase[SIZE];//tableau de saisie
+	char mots[SIZE];//tableau imprimé
+	int n = 1;//nombre de mots
+
+	fgets(phrase , SIZE , stdin);
+	
+	for (int a = 0; a <= SIZE; a++)
+	{
+		if ((phrase[a] == ' ')&&(isalnum(phrase[a-1])))
+		{
+			mots[a] = '\n';
+			n++;
+		}
+		else
+		{
+			mots[a] = phrase[a];
+		}
+	}
+
+	printf("%s", mots);
+
+	printf("\nil y a %d argument\n", n);
+
+	system("PAUSE");
+	return (EXIT_SUCCESS);
+}
